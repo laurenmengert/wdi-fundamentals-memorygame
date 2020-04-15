aefd87b4184bc7411cc1b1a2fb092f03bccbac58
Lauren Mengert <laurenmengert@Laurens-MacBook-Pro.local>
2020-04-15 09:50:25 -0600

    Created game logic

---
+++ memory-game/JS/main.js
@@ -0,0 +1,9 @@
+console.log("Up and running!");
+let cardOne = "queen";
+let cardTwo = "queen";
+let cardThree = "king";
+let cardFour = "king";
+
+console.log ("User Flipped" + " " + cardOne);
+console.log ("User Flipped" + " " + cardThree);
+
